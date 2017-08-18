eff9f30f1fc2d90f03513d6282cc7f5978152248
david.coxon <david.coxon@eisl.eu.com>
2017-08-18 16:00:17 +0100

    Add ternary operator base class and test case operator.

---
+++ decl/Expressions/Operators/Ternary/TestCase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace declang.Expressions
+{
+    internal class TestCase : TernaryOperator
+    {
+        public TestCase(string operatorString, IExpression firstOperand, IExpression secondOperand, IExpression thirdOperand, string format = "{0}:{1}{{ {2} }}") 
+            : base(operatorString, firstOperand, secondOperand, thirdOperand, format) { }
+
+        public override ExpressionResult Evaluate(IDictionary<string, ExpressionResult> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ decl/Expressions/Operators/TernaryOperator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace declang.Expressions
+{
+    internal abstract class TernaryOperator : Operator
+    {
+        protected IExpression firstOperand;
+        protected IExpression secondOperand;
+        protected IExpression thirdOperand;
+
+        public IExpression FirstOperand => firstOperand;
+        public IExpression SecondOperand => secondOperand;
+        public IExpression ThirdOperand => thirdOperand;
+
+        public TernaryOperator(string operatorString, IExpression firstOperand, IExpression secondOperand, IExpression thirdOperand, string format = "{0}{1}{2}") 
+            : base(format)
+        {
+            this.firstOperand = firstOperand;
+            this.secondOperand = secondOperand;
+            this.thirdOperand = thirdOperand;
+        }
+
+        public override string ToString()
+        {
+            return String.Format(format, firstOperand, secondOperand, thirdOperand);
+        }
+    }
+}
