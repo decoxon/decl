194e3eb595dee828e6f1065731557c491a32bdfd
decoxon <decoxon@gmail.com>
2017-08-21 17:31:56 +0100

    Add ExpressionDefinition class.

---
+++ decl/Parsing/ExpressionDefinition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace declang.Parsing
+{
+    internal class ExpressionDefinition
+    {
+        public ExpressionType Type { get; set; }
+        public int Precedence { get; set; }
+        public char[] TriggerCharacters { get; set; }
+
+        public bool IsTriggerCharacter(char c)
+        {
+            for(var i = 0; i < TriggerCharacters.Length; i++)
+            {
+                if (c == TriggerCharacters[i])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
