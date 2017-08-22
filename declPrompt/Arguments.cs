a3692da6626882627c426967f01fad56accadf10
david.coxon <david.coxon@eisl.eu.com>
2017-08-22 11:01:18 +0100

    Add reading script from file on the command line.

--- declPrompt/Arguments.cs
+++ declPrompt/Arguments.cs
@@ -10,10 +10,13 @@ namespace declPrompt
 {
     class Arguments
     {
-        [Option('s', "statement", HelpText ="The statement to parse.")]
+        [Option('s', "statement", HelpText = "The statement to parse.", MutuallyExclusiveSet = "stringSource")]
         public string Statement { get; set; }
 
-        [Option('i', "interactive", HelpText ="Enter interactive mode", MutuallyExclusiveSet ="interactive")]
+        [Option('i', "interactive", HelpText = "Enter interactive mode", MutuallyExclusiveSet = "interactive")]
         public bool IsInteractiveModeRequested { get; set; }
+
+        [Option('f', "file", HelpText = "The file to be loaded", MutuallyExclusiveSet ="fileSource")]
+        public string FilePath { get; set; }
     }
 }
